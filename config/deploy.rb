00b718a6fe064bcc0937d001526edad13c32bdd2
Greg Leuch <contact@gleu.ch>
2014-07-27 22:28:07 -0400

    add viewport meta tag. include rails env in deploy rake.

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,8 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'cache:clear'
+        rails_env = fetch(:rails_env, 'production')
+        execute :rake, "cache:clear RAILS_ENV=#{rails_env}"
       end
     end
   end
